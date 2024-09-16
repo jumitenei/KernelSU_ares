380083d85042ad169ccc3f13158e1b305d77a75a
SunsetDreamer <134821284+jumitenei@users.noreply.github.com>
2024-09-16 23:44:54 +0800

    Update binder.c

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3687,18 +3687,6 @@ static void binder_transaction(struct binder_proc *proc,
 		target_proc = target_thread->proc;
 		target_proc->tmp_ref++;
 		binder_inner_proc_unlock(target_thread->proc);
-   		if (start_rekernel_server() == 0) {
-			if (target_proc
-				&& (NULL != target_proc->tsk)
-				&& (NULL != proc->tsk)
-				&& (task_uid(target_proc->tsk).val <= MAX_SYSTEM_UID)
-				&& (proc->pid != target_proc->pid)
-				&& line_is_frozen(target_proc->tsk)) {
-     				char binder_kmsg[PACKET_SIZE];
-                       	snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=reply,oneway=0,from_pid=%d,from=%d,target_pid=%d,target=%d;", proc->pid, task_uid(proc->tsk).val, target_proc->pid, task_uid(target_proc->tsk).val);
-         			send_netlink_message(binder_kmsg, strlen(binder_kmsg));
-			}
-   		}
 #ifdef BINDER_WATCHDOG
 		e->service[0] = '\0';
 #endif
